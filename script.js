ba6a1d7936f46422ecd4d1a0bda96f666b2fcee7
Sherin <68326500+Sherinsedalo@users.noreply.github.com>
2024-06-04 23:15:34 +0200

    Adding the styling and contact form

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ function closeSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+
+
+var string = "I am a Junior Frontend developer"; /* type your text here */
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+	if (array.length > 0) {
+		document.getElementById("typing").innerHTML += array.shift();
+	} else {
+		clearTimeout(timer);
+			}
+	loopTimer = setTimeout('frameLooper()',70); /* 70 is for speed */
+
+}
+frameLooper();
